55a1f8bf0a2c2f5e51d46b3b5982499790f1c219
82105 <82105@192.168.219.101>
2022-07-15 10:28:51 +0900

    0715 daily java

--- YD_STUDY/src/co/study/solve/Login.java
+++ YD_STUDY/src/co/study/solve/Login.java
@@ -17,21 +17,20 @@ public class Login {
 
 		Scanner scn = new Scanner(System.in);
 
-		System.out.println("아이디를 로그인하세요");
-		String id = scn.nextLine();
+		String id = scn.nextLine();				//문자열(String) 타입으로 id를 키보드 입력 받음
+		String strPwd = scn.nextLine();		    //문자열(String) 타입으로 strpwd를 키보드 입력받음
 
-		System.out.println("패스워드를 로그인하세요");
-		String pw = scn.nextLine();
-		int pwInt = Integer.parseInt(pw);
+		int pw = Integer.parseInt(strPwd);		//문자열 > 정수(int)타입으로 변환. Integer.parseInt
 
-		if (id.equals("yedam")) {
-			if (pwInt == 1234) {
-				System.out.println("로그인 성공");
+		if (id.equals("yedam")) {						// if~ id가 yedam과 일치할 때   
+														// String 은 변수.equals(문자열)로 비교
+			if (pw == 1234) {							// int는 정수이므로 연산자 ==로 비교 
+				System.out.println("로그인 성공~~~!!!");
 			} else {
-				System.out.println("로그인 실패 : 패스워드 실패");
-			}
+				System.out.println("로그인 실패입니다!");	// 이중 if 구문 이용하여 id가 yedam 이면서 
+			}											// 비밀번호 일치, 불일치 경우 나눠줌 
 		} else {
-			System.out.println("로그인 실패 : 아이디 오류");
+			System.out.println("아이디를 다시 확인하세요");   // id가 yedam이 아닐 경우도 포함
 		}
 	}
 
