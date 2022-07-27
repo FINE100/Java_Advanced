2a0282be3e2c2ee1fdf3efe2763d0249f150e359
cococ <cococ@192.168.219.101>
2022-07-28 00:04:53 +0900

    .

--- YD_STUDY/src/co/study/solve/Employee.java
+++ YD_STUDY/src/co/study/solve/Employee.java
@@ -1,10 +1,42 @@
 package co.study.solve;
 
 public class Employee {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	//필드
+	public String name;
+	public int salary;
+	
+	//생성자
+	public Employee() {
+		
 	}
 
-}
+	// 메소드
+	public void getInformation(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+		
+    }
+	public void print() {
+		System.out.println("수퍼클래스");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [toString()=" + super.toString() + "]";
+	}
+	
+	
+	
+	}
+
+
+
